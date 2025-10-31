16734349d6647a1f3c5fdd41364e87684be6ecd1
Shabnam <sbeiraghian@my.bcit.ca>
2025-10-31 16:25:21 -0700

    next.js

---
+++ src/components/MyNavBar.js
@@ -0,0 +1,48 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+        <Link href="/">Home</Link>
+      </NavigationMenuLink>
+        </NavigationMenuItem>
+        
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/projects">Projects</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Resume</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink href="/resume">Latex</NavigationMenuLink>
+            <NavigationMenuLink href="/resume/pdf">PDF</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/login">Login</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+      </NavigationMenuList>
+    </NavigationMenu>
+
+  );
+}
